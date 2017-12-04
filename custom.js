6bbf813d0d67eca2dcbf40b4bf7f2d0e04407875
WanjingZhang <wanjing.zhang@wlt.com>
2017-12-04 10:36:05 +0800

    php

--- custom.js
+++ custom.js
@@ -15,3 +15,12 @@ console.log(p1 instanceof Point);
 var p2 = new (Point)(10,20);
 p2.print();
 console.log(p2 instanceof Point);
+
+var a = ["world"];
+var value = a[0];
+a[1] = 3.14;
+i =2;
+a[i] =3;
+a[i+1] ="hello";
+a[a[i]] = a[i+1] + " "+ a[0];
+console.log(a);
