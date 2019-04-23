ca0614b11c70443e15765fb0cbcd9257a3aa209a
wanjingz <95616085@qq.com>
2019-04-23 15:30:08 +0800

    add the gird items

--- 03REACT/React_Building_Styles_CSS/src/App.js
+++ 03REACT/React_Building_Styles_CSS/src/App.js
@@ -6,16 +6,24 @@ import GridItem from './Grid/GridItem';
 import Rocket from './Rocket/Rocket';
 import Smoke from './Rocket/Smoke';
 
+import facts from './NASA_facts.json';
+
 //styles
 import styles from './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      facts,
+    }
+  }
   render() {
     return (
       <div className={styles.app}> 
         <Title />
         <Background />
-        <Grid />
+        <Grid facts={this.state.facts}/>
         <Rocket />
         <Smoke />
       </div>
--- 03REACT/React_Building_Styles_CSS/src/Grid/Grid.js
+++ 03REACT/React_Building_Styles_CSS/src/Grid/Grid.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import GridItem from './GridItem';
+import styles from './Grid.css';
 
 export default class Gird extends React.Component {
+  renderFacts() {
+    return this.props.facts.map(item =>
+      <GridItem
+        key={item.id}
+        fact={item}
+      />
+      )
+  }
   render() {
     return (
-      <div>
-        <h1>Gird</h1>
+      <div className={styles.grid}>
+        {this.renderFacts()}
       </div>
     );
   }
