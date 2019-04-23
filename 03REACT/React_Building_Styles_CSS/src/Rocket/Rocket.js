fff2b15df3be1c9d679853ed37f2e6e3dc16682c
wanjingz <95616085@qq.com>
2019-04-23 16:03:41 +0800

    add the title and rocket

--- 03REACT/React_Building_Styles_CSS/src/Rocket/Rocket.js
+++ 03REACT/React_Building_Styles_CSS/src/Rocket/Rocket.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Rocket.css';
 
 export default class Rocket extends React.Component {
   render() {
     return (
       <div>
-        <h1>Rocket</h1>
+        <img src="/shuttle.png" alt="shuttle" className={styles.img} />
       </div>
     );
   }
