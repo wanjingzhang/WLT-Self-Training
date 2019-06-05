fc361ff892174bcebda2e88e65e434b2b71dfbdb
wanjingz <95616085@qq.com>
2019-06-05 14:28:43 +0800

    add the audio test

--- 02HTML/4Canvas/DEMO/SNOWMAN/js/sound.js
+++ 02HTML/4Canvas/DEMO/SNOWMAN/js/sound.js
@@ -1,16 +1,22 @@
 
-var soundJump = document.getElementById('soundJump');
-var soundScore = new Audio('music/point.ogg');
-var soundHit = new Audio('music/hit.ogg');
-var soundDie = new Audio('music/die.ogg');
-var soundSwoosh = new Audio('music/swooshing.ogg');
+var soundJump = new Audio('music/wing.mp3');
+var soundScore = new Audio('music/point.mp3');
+var soundHit = new Audio('music/hit.mp3');
+var soundDie = new Audio('music/die.mp3'); 
 
 var channel_max = 10; //频道数量
 var audiochannels = new Array();
-for (a = 0; a < channel_max; a++){
-    audiochannels[a] = new Array();
-    audiochannels[a]['channel'] = new Audio();
+for (a = 0; a < channel_max; a++){ 
+    // audiochannels[a]['channel'] = new Audio();
+    // audiochannels[a]['finished'] = -1;
+
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute("id", "audio"+a);
+    document.body.appendChild(audioElement);
+    audiochannels.push(audioElement);
     audiochannels[a]['finished'] = -1;
+    audiochannels[a]['channel'] = audioElement;
+
     console.log("audio", a, audiochannels[a]['channel'] );
 }
 
@@ -26,12 +32,17 @@ function play_sound(s) {
             audiochannels[a]['finished'] = thistime.getTime() + s.duration * 1000;
             audiochannels[a]['channel'].src = s.src;
             audiochannels[a]['channel'].load();
+            audiochannels[a]['channel'].volume = 1;
             var playPromise = audiochannels[a]['channel'].play();
             if (playPromise !== undefined) {
                 playPromise.then(function () {
-                    console.log("Automatic playback stared~!")
+                    console.log("Automatic playback stared~!") 
                 }).catch(function (error) {
-                    console.log("Automatic playback failed")
+                    // console.log The request is not allowed by the user agent or the platform in the current context, possibly because the user denied permission.
+                    // Auto-play was prevented
+                    // Show a UI element to let the user manually start playback
+                    console.log("Automatic playback failed");
+                    console.log(playPromise);
                 });
             }
             break;
