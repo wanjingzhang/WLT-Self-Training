107cdfda6355635ef4b16690c1b575aab7a43f4b
WanJing Zhang (WLT CN) <WanJing.Zhang@wlt.com>
2019-07-03 11:10:36 +0800

    add the bg parallax scroll for background

--- 02HTML/4Canvas/DEMO/SNOWMAN/js/libs/Route.js
+++ 02HTML/4Canvas/DEMO/SNOWMAN/js/libs/Route.js
@@ -1,6 +1,7 @@
 SNOW.Route = function (x, y, width ) {
     this.init = function () {
         this.x = x; 
+        this.x2 = x;
         this.y = y;
         this.oldx = x;   
         // this.img = new Image();
@@ -15,9 +16,13 @@ SNOW.Route = function (x, y, width ) {
 
     this.update = function () {
         this.x -= SNOW.Speed;
-        if (this.x < -this.width) { 
-            this.respawn();
-        }     
+        this.x2 -= SNOW.Speed * 2;
+        if (this.x < - this.width) { 
+            this.respawn(1);
+        } 
+        if (this.x2 < - this.width) {
+            this.respawn(2);
+        } 
     }
 
     this.render = function () {
@@ -26,16 +31,20 @@ SNOW.Route = function (x, y, width ) {
         // SNOW.Draw.Image(this.img, this.x + this.width, this.y);
         
         // trees
-        SNOW.Draw.Image(this.tree, this.x, SNOW.HEIGHT - 223);
-        SNOW.Draw.Image(this.tree, this.x + this.width, SNOW.HEIGHT - 223); 
-
+        SNOW.Draw.Image(this.tree, this.x, SNOW.HEIGHT - 240);
+        SNOW.Draw.Image(this.tree, this.x + this.width, SNOW.HEIGHT - 240); 
+        
         // trees
-        SNOW.Draw.Image(this.trees, this.x, SNOW.HEIGHT - 253);
-        SNOW.Draw.Image(this.trees, this.x + this.width, SNOW.HEIGHT - 253); 
+        SNOW.Draw.Image(this.trees, this.x2, SNOW.HEIGHT - 253);
+        SNOW.Draw.Image(this.trees, this.x2 + this.width, SNOW.HEIGHT - 253); 
         
     }
 
-    this.respawn = function () {
-        this.x = this.oldx;
+    this.respawn = function (id) {
+        if (id === 1) {
+            this.x += this.width; 
+        } else if (id === 2){
+            this.x2 += this.width;
+        }
     }
 }
