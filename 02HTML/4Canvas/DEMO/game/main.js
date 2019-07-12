7011fb5ce509b945ef8538096f9925286c54d03d
WanJing Zhang (WLT CN) <WanJing.Zhang@wlt.com>
2019-07-12 10:30:37 +0800

    finish game bg

--- 02HTML/4Canvas/DEMO/game/main.js
+++ 02HTML/4Canvas/DEMO/game/main.js
@@ -4,16 +4,21 @@ function draw() {
     ctx.fillStyle = "#001d2e";
     ctx.fillRect(0, 0, ww, wh);
 
+    var span = 50;
     ctx.beginPath();
-    for (var i = 0; i < ww; i += 20){
+    for (var i = 0; i < ww; i += span){ // 竖线 ||
         ctx.moveTo(i, 0);
         ctx.lineTo(i, wh);
     }
-    ctx.strokeStyle = "white";
+    for (var i = 0; i < wh; i += span) { // 横线 =
+        ctx.moveTo(0, i);
+        ctx.lineTo(ww, i);
+    }
+    ctx.strokeStyle = "rgba(255,255,255,0.2)";
     ctx.stroke();
 
-    ctx.fillStyle = "white";
-    ctx.fillRect(ship.x, ship.y, 50, 50);
+    // ctx.fillStyle = "white";
+    // ctx.fillRect(ship.x, ship.y, 50, 50);
     requestAnimationFrame(draw);
 
 }
