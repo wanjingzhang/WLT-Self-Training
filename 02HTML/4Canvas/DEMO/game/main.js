ea7394fbb49a7e51113bfe4770e6c83a42b7ae77
WanJing Zhang (WLT CN) <WanJing.Zhang@wlt.com>
2019-07-12 14:51:25 +0800

    add rotator and block

--- 02HTML/4Canvas/DEMO/game/main.js
+++ 02HTML/4Canvas/DEMO/game/main.js
@@ -1,4 +1,9 @@
-var canvas, ctx, ww, wh, ship;
+var canvas, ctx, ww, wh, ship = {
+    x: 0,
+    y: 0,
+    deg: 0,
+    r: 60
+};
 
 function draw() {
     ctx.fillStyle = "#001d2e";
@@ -18,16 +23,24 @@ function draw() {
     ctx.strokeStyle = "rgba(255,255,255,0.2)";
     ctx.stroke();
 
-    ctx.save(); // 储存画布的状态 #1
-
-    ctx.translate(ww/2,wh/2); // 移动 中心点 
     // 船
+    ctx.save(); // 储存画布的状态 #1 
+    ctx.translate(ww / 2, wh / 2); // 移动 中心点 
+    ctx.rotate(ship.deg); //旋转 物体
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(100,-25/2,25,25); //中心点
+
     ctx.beginPath();
     ctx.arc(0, 0, ship.r, 0, Math.PI * 2); //2pai
     ctx.strokeStyle = "white";
     ctx.lineWidth = 20;
-    ctx.stroke();
-    
+
+   
+    ctx.stroke(); 
+    // 发光
+    ctx.shadowBlur = 20;
+    ctx.shadowColor = "white";
     // 线
     var i = 3;
     while (i--) {
@@ -46,8 +59,14 @@ function draw() {
 
     ctx.fillStyle = "white";
     ctx.fillRect(ship.x, ship.y, 50, 50);
-    requestAnimationFrame(draw);
+    requestAnimationFrame(draw); 
+    
+}
 
+function update() {
+    ship.x += 0.1;
+    ship.y += 0.5;
+    ship.deg += 0.05;
 }
 
 function init() {
@@ -57,12 +76,12 @@ function init() {
     wh = window.innerHeight;
     canvas.width = ww;
     canvas.height = wh;
-    ship = {
-        x: 0,
-        y: 0,
-        deg: 0,
-        r: 100
-    }
+    
+    ship.deg = 45;
+    ship.x = Math.random() * ww;
+    ship.y = Math.random() * wh;
+
+    setInterval(update, 30);
     draw();
 }
 
