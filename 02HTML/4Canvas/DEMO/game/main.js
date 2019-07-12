ee8ec0b430a097ac4bb75c9dc333903f68a787b2
WanJing Zhang (WLT CN) <WanJing.Zhang@wlt.com>
2019-07-12 16:44:58 +0800

    add the bullets

--- 02HTML/4Canvas/DEMO/game/main.js
+++ 02HTML/4Canvas/DEMO/game/main.js
@@ -1,17 +1,84 @@
-var canvas, ctx, ww, wh,
-    ship = {
-    x: 0,
-    y: 0,
-    deg: 0,
-    r: 60
-    };
+var canvas, ctx, ww, wh, ship,ship2,bullets=[];
 
 var mousePos = {
     x: 0,
     y: 0
 }
 
-function draw() {
+var degToPi = Math.PI * 2 / 360;
+
+class Ship{
+    constructor(args) {
+        let def = {
+            x: 0,
+            y: 0,
+            r: 50,
+            deg: 50 * degToPi
+        }
+        Object.assign(def, args);
+        Object.assign(this, def);
+    }
+    draw() {
+        ctx.save();
+        ctx.rotate(this.deg); //旋转 物体
+
+        ctx.fillStyle = "white";
+        ctx.fillRect(100, -25 / 2, 25, 25); //中心点
+
+        ctx.beginPath();
+        ctx.arc(0, 0, this.r, 0, Math.PI * 2); //2pai
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 12;
+
+
+        ctx.stroke();
+        // 发光
+        ctx.shadowBlur = 20;
+        ctx.shadowColor = "white";
+        // 线
+        var i = 3;
+        while (i--) {
+            // 绘制一条直线
+            ctx.lineWidth = 5;
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(0, -this.r);
+            ctx.stroke();
+
+            // 旋转画布 360/3=120度
+            ctx.rotate(Math.PI * 2 / 3);
+        }
+        ctx.restore();
+    }
+}
+
+class Bullet{
+    constructor(args) {
+        let def = {
+            x: 0,
+            y: 0,
+            v: {
+                x: 0,
+                y: 0,
+            }
+        }
+        Object.assign(def, args);
+        Object.assign(this, def);
+    }
+    update() {
+        this.x += this.v.x;
+        this.y += this.v.y;
+    }
+    draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, 10, 10);
+        ctx.restore();
+    }
+}
+
+function draw() { 
     ctx.fillStyle = "#001d2e";
     ctx.fillRect(0, 0, ww, wh);
 
@@ -32,49 +99,33 @@ function draw() {
     // 船
     ctx.save(); // 储存画布的状态 #1 
     ctx.translate(ww / 2, wh / 2); // 移动 中心点 
-    ctx.rotate(ship.deg); //旋转 物体
-
-    ctx.fillStyle = "white";
-    ctx.fillRect(100,-25/2,25,25); //中心点
-
-    ctx.beginPath();
-    ctx.arc(0, 0, ship.r, 0, Math.PI * 2); //2pai
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 12;
-
-   
-    ctx.stroke(); 
-    // 发光
-    ctx.shadowBlur = 20;
-    ctx.shadowColor = "white";
-    // 线
-    var i = 3;
-    while (i--) {
-        // 绘制一条直线
-        ctx.lineWidth = 5;
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.lineTo(0, -ship.r);
-        ctx.stroke(); 
-
-        // 旋转画布 360/3=120度
-        ctx.rotate(Math.PI*2/3);
-    }
-     
+    ship.draw();   
     ctx.restore(); // 还原画布的状态 回到上一个存档点 #1 
+
+    bullets.forEach(b => b.draw()); 
 
     ctx.fillStyle = "white";
     ctx.fillRect(ship.x, ship.y, 50, 50);
     requestAnimationFrame(draw); 
     
 }
-
-function update() {
-    // ship.x += 0.1;
-    // ship.y += 0.5;
-    // ship.deg += 0.05;
-    ship.deg = mousePos.x/50;
-    console.log(mousePos);
+var time = 0;
+function update() { 
+    ship.deg = mousePos.x / 50; 
+    time++; 
+    if (time % 30 == 0) {
+        console.log('add Bullet');
+        let b = new Bullet({
+            x: 50,
+            y: 50,
+            v: {
+                x: 1,
+                y: 1
+            }
+        });
+        bullets.push(b);
+    }
+    bullets.forEach(b => b.update());
 }
 
 function init() {
@@ -84,11 +135,11 @@ function init() {
     wh = window.innerHeight;
     canvas.width = ww;
     canvas.height = wh;
-    
-    ship.deg = 45;
-    ship.x = Math.random() * ww;
-    ship.y = Math.random() * wh;
-
+     
+    ship = new Ship({
+        deg: 45 * degToPi,
+        r:120
+    })  
     setInterval(update, 30);
     draw();
 
