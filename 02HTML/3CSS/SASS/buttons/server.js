d7187677db5ec43fa9d76f00e850dba4b41e2426
WanJing Zhang (WLT CN) <WanJing.Zhang@wlt.com>
2019-08-30 15:56:38 +0800

    add the thirteenth button

--- 02HTML/3CSS/SASS/buttons/server.js
+++ 02HTML/3CSS/SASS/buttons/server.js
@@ -1,19 +1,26 @@
 var connect = require('connect');
-var sassMiddleware = require('node-sass-middleware');
-
+var sassMiddleware = require('node-sass-middleware'); 
+var autoprefixer = require('autoprefixer');
+var browserslist = require('browserslist');
 var srcPath = __dirname + '/sass';
 var destPath = __dirname + '/public/styles';
 
-var serveStatic = require('serve-static')
+var serveStatic = require('serve-static'); 
 var http = require('http');
 var port = process.env.PORT || 8000;
 var app = connect();
-app.use('/styles', sassMiddleware({
+app.use('/styles', sassMiddleware({ 
+  plugins: [
+    /* Plugins */
+    autoprefixer({
+      grid: "autoplace", 
+    })
+  ],
   src: srcPath,
   dest: destPath,
   debug: true,
-  outputStyle: 'expanded'
-}));
+  outputStyle: 'expanded' 
+})); 
 app.use('/',
   serveStatic('./public', {})
 );
@@ -21,3 +28,5 @@ http.createServer(app).listen(port);
 console.log('Server listening on port ' + port);
 console.log('srcPath is ' + srcPath);
 console.log('destPath is ' + destPath);
+
+console.log(autoprefixer().info());
