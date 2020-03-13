d3076019719d756d97e8304cf6aee0434f50d3d6
WanJing Zhang (WLT CN) <WanJing.Zhang@wlt.com>
2020-03-13 18:02:47 +0800

    add the stacking

--- 02HTML/2JS/DEMO/flipcard/main.js
+++ 02HTML/2JS/DEMO/flipcard/main.js
@@ -1,122 +1,131 @@
-; (function (window) { 
-    // Game
-    var Game = function (el, option) {
-        this.el = document.getElementById(el);
-        this.option = option;
-        // Info section 
-        this.info_div = document.createElement('div');
-        this.info_div.id = "info_div";
+;(function(window){
+	// Game
+	var Game = function(el, option){
+		this.el = document.getElementById(el);
+		this.option = option;
+		// 	Info section
+	
+		this.info_div = document.createElement('div');
+		this.info_div.id = "info_div";
+		// 	Deck
+	
+		this.deck_div = document.createElement("div");
+		this.deck_div.id = "deck_div";
+		this.gameDeck = new Deck(this.deck_div, option);
+		this.gameDeck.buildDeck();
 
-        // Deck 
-        this.deck_div = document.createElement('div');
-        this.deck_div.id = "deck_div";
-        this.gameDeck = new Deck(this.deck_div, option);
-        this.gameDeck.buildDeck();
+		var shuffleBtn =document.createElement("button");
+		shuffleBtn.innerHTML = "Shuffle";
+		shuffleBtn.onclick = this.gameDeck.shuffle.bind(this);
+		
+		this.info_div.appendChild(shuffleBtn);
+		// 	Discard Pile
+		// 	Rules
 
-        var shuffleBtn = document.createElement("button");
-        shuffleBtn.innerHTML = "Shuffle";
-        shuffleBtn.onclick = this.gameDeck.shuffle.bind(this); // 绑定高级别的 game object 对象
-         
-        this.info_div.appendChild(shuffleBtn);
-        //     Discard Pile
-        //     Rules
+		this.el.appendChild(this.info_div);
+		this.el.appendChild(this.deck_div);
 
-        this.el.appendChild(this.info_div);
-        this.el.appendChild(this.deck_div);
-    } 
+	}
+	
 
-    // Deck
-    var Deck = function (deck_div, option) {
-        this.deckData = option.data;
-        this.buildDeck = function () {
-            var parentFrag = document.createDocumentFragment();
-            deck_div.innerHTML = "";
-            for (var i = this.deckData.length - 1; i >= 0; i--){
-                var card = new Card();
-                card.id = "card" + i;
-                card.data = this.deckData[i];
-                card.buildCard(parentFrag);
-                
-            }
-            deck_div.appendChild(parentFrag);
-        }
-        // this.shuffle();
-    }
-    // Cards 
-    // ----
-    // shuffle 洗牌代码把数组里面的数据打乱
-    Deck.prototype.shuffle = function () {
-        // console.log(this);
-        var cardsToShuffle = this.gameDeck.deckData;
-        var m = cardsToShuffle.length, t, i;
-        while (m) {
-            i = Math.floor(Math.random() * m--);
-            t = cardsToShuffle[m];
-            cardsToShuffle[m] = cardsToShuffle[i];
-            cardsToShuffle[i] = t;
-        }
-        this.gameDeck.deckData = cardsToShuffle;
-        this.gameDeck.buildDeck(this.deck_div);
-    }
-    // stack
-     
-    // Cards
-    var Card = function () {
-        this.id = "";
-        this.data = "";
-        this.cardCont = document.createElement('div');
-        this.cardCont.className = "card_containter";
-        this.cardFront = document.createElement('div');
-        this.cardFront.className = "card_front";
-        this.cardBack = document.createElement('div');
-        this.cardBack.className = "card_back";
-        this.buildCard = function (parentFrag) {
-            var flipDiv = document.createElement('div'),
-                frontValDiv = document.createElement('div'),
-                backValpDiv = document.createElement('div'),
-                catDiv = document.createElement('div');
-            flipDiv.className = "flip";
-            frontValDiv.className = "front_val";
-            backValpDiv.className = "back_val";
-            catDiv.className = "cat_val";
+	// Deck
+	var Deck = function(deck_div, option){
+		this.deckData = option.data;
+		this.buildDeck = function(){
+			var parentFrag = document.createDocumentFragment();
+			deck_div.innerHTML = "";
+			for (var i = this.deckData.length - 1; i >= 0; i--) {
+				var card = new Card();
+				card.id = "card-" + i;
+				card.data = this.deckData[i];
+				card.buildCard(parentFrag);
+			}
+			deck_div.appendChild(parentFrag);
+			this.stack(deck_div);
+		}
+		
+		
+	}
+	// 	Cards
+	// 	----
+	// 	shuffle
+	Deck.prototype.shuffle = function(){
+		var cardsToShuffle = this.gameDeck.deckData;
+		var m = cardsToShuffle.length, t, i;
+		while(m){
+			i = Math.floor(Math.random() * m--);
+			t = cardsToShuffle[m];
+			cardsToShuffle[m] = cardsToShuffle[i];
+			cardsToShuffle[i] = t;
+		}
+		this.gameDeck.deckData = cardsToShuffle;
+		this.gameDeck.buildDeck(this.deck_div);
+	}
+	// 	stack 堆栈保护变量 
+	Deck.prototype.stack = function(deck_div){
+		var cards = deck_div.children;
+		for (var i = cards.length - 1; i >= 0; i--) {
+			cards[i].style.top = i + "px";
+			cards[i].style.left = i + "px";
+			cards[i].classList.add("stacked_card");
+		}
+	}
+	// Card
+	var Card = function(){
+		this.id = "";
+		this.data = "";
+		this.cardCont = document.createElement("div");
+		this.cardCont.className = "card_container";
+		this.cardFront = document.createElement("div");
+		this.cardFront.className = "card_front";
+		this.cardBack = document.createElement("div");
+		this.cardBack.className = "card_back";
+		this.buildCard = function(parentFrag){
+			var flipDiv = document.createElement("div"),
+				frontValDiv = document.createElement("div"),
+				backValDiv = document.createElement("div"),
+				catDiv = document.createElement("div");
+			flipDiv.className = "flip";
+			frontValDiv.className = "front_val";
+			backValDiv.className = "back_val";
+			catDiv.className = "cat_val";
+			// 	val
+			frontValDiv.innerHTML = this.data.q;
+			backValDiv.innerHTML = this.data.a;
+			// 	suit
+			catDiv.innerHTML = this.data.category;
 
-            frontValDiv.innerHTML = this.data.q;
-            backValpDiv.innerHTML = this.data.a;
-            catDiv.innerHTML = this.data.category;
+			this.cardFront.appendChild(frontValDiv);
+			this.cardFront.appendChild(catDiv);
+			this.cardBack.appendChild(backValDiv);
 
-            this.cardFront.appendChild(frontValDiv);
-            this.cardFront.appendChild(catDiv);
-            this.cardBack.appendChild(backValpDiv);
-            
-            flipDiv.appendChild(this.cardFront);
-            flipDiv.appendChild(this.cardBack);
+			flipDiv.appendChild(this.cardFront);
+			flipDiv.appendChild(this.cardBack);
 
-            this.cardCont.id = this.id;
-            this.cardCont.appendChild(flipDiv);
-            this.cardCont.onclick = function (e) {
-                // console.log(e.target, e.currentTarget); // 当前目标，当前父容器
-                // console.log(e.currentTarget.className, e.currentTarget.classList); // 样式、样式表
-                // console.log(this); // 当前父容器
+			this.cardCont.id = this.id;
+			this.cardCont.appendChild(flipDiv);
+			// 	----
+			// 	flip
+			this.cardCont.onclick = cardClick; 
+			parentFrag.appendChild(this.cardCont);
+		}
+	}
+	
+	var cardClick = (function(e){
+		var counter = 0;
+		return function (e) {
+			e.currentTarget.classList.toggle("flip_card");
+			e.currentTarget.classList.toggle("slide_over");
+			e.currentTarget.style.zIndex = counter;
+			counter++;
+		}
+	})() 
+	// Discard Pile
+	var DiscardPile = function(){
 
-                e.currentTarget.classList.toggle("flip_card");
-                e.currentTarget.classList.toggle("slide_over");
-
-            }
-            parentFrag.appendChild(this.cardCont);
-        }
-    }
-    //     val
-    //     suit
-    //     ----
-    //     flip
-
-    // Discard Pile
-    var Discard = function () {
-        
-    }
-    // Holders
-    //     ----
-    //     accept or reject
-    window.Game = Game;
-})(window)
-
+	}
+	// 	Holders
+	// 	----
+	// 	accept or reject
+	window.Game = Game;
+})(window);
